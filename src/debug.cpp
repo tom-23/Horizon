f457974c8233af18f4ecb47c1cfc2fdc4c50791f
Tom Butcher <thomasbutcher15@gmail.com>
2020-06-07 03:08:27 +0100

    Debug implementation

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,6 +1,24 @@
 #include "debug.h"
+#include <QDebug>
 
-Debug::Debug()
-{
+namespace debug {
+    void setDebugLevel(int level) {
+        overallDebugLevel = level;
+    }
+
+    int getDebugLevel() {
+        return overallDebugLevel;
+    }
+
+    void out(int level, std::string message) {
+        if (level == 1 && overallDebugLevel >= 1) {
+            std::cout << "[ERROR] " << message << std::endl;
+        } else if (level == 2 && overallDebugLevel >= 2) {
+            std::cout << "[WARNING] " << message << std::endl;
+        } else if (level == 3 && overallDebugLevel >= 3) {
+            std::cout << "[INFO] " << message << std::endl;
+        }
+
+    }
 
 }
