cf40f019af2ee3dde780f43c8707099a6cfb2efd
Tom Butcher <thomasbutcher15@gmail.com>
2020-06-18 00:24:30 +0100

    Moved from TitilliumWeb to Raleway for the main font.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
-    QFontDatabase::addApplicationFont(":/fonts/fonts/TitilliumWeb.ttf");
-    QFontDatabase::addApplicationFont(":/fonts/fonts/TitilliumWeb-SemiBold.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Raleway/Raleway-Regular.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Raleway/Raleway-Medium.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Raleway/Raleway-SemiBold.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Raleway/Raleway-Bold.ttf");
+
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Raleway/Raleway-Italic.ttf");
 
     debug::setDebugLevel(3);
     debug::out(3, "Horizon Digital Audio Workstation");
